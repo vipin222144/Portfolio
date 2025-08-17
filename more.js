a75d38b0ddcfe2537741daa4d323e0452a2c90db
vipin tiwari <154210599+vipin222144@users.noreply.github.com>
2025-08-17 12:21:37 +0530

    Add files via upload

---
+++ more.js
@@ -0,0 +1,15 @@
+function more(){
+    document.getElementById("more").innerHTML=` <span id="more2"> Over the years, I have built a solid foundation in programming and web development, with skills in
+                    HTML, CSS,JavaScript, Python, Java, C, and SQL.
+                    My goal is to continuously improve my technical expertise while also enhancing my problem-solving
+                    and logical thinking abilities.
+                    I enjoy working on projects that allow me to apply my knowledge creatively, whether it’s developing
+                    user-friendly websites or writing efficient, structured code.
+
+                    I believe in continuous learning and adaptability,
+                    which are essential in today’s fast-evolving tech world.
+                    Beyond academics,I value teamwork, discipline, and dedication, and I always aim to deliver my best
+                    in every task.
+                    My ambition is to work in the IT industry where I can contribute to innovative solutions,
+                    expand my professional skills, and grow into a versatile and resourceful</span>`;
+}
